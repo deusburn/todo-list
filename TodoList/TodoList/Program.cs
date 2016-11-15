71ef38881b8654c9581255cbc689b3aa00b2c90f
deusburn <sky_burn@ukr.net>
2016-11-15 16:06:22 +0200

    Added user commands

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -10,6 +10,42 @@ namespace TodoList
     {
         static void Main(string[] args)
         {
+            int userReaction = 0;
+            do
+            {
+                Console.WriteLine(
+                    "------------------------------- \n" +
+                    " The program has a functional : \n\n" +
+                    " 1. Add post ; \n" +
+                    " 2. Delete post ; \n" +
+                    " 3. Show all posts ; \n" +
+                    " 4. Exit ; \n\n" +
+                    " Please enter the number :");
+
+                userReaction = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (userReaction)
+                {
+                    case 1:
+                        Console.WriteLine("You choosed Add post. Enter your line below");
+                        var one = Console.ReadLine();
+                        Console.WriteLine("You wrote: {0}", one);
+                        Console.WriteLine("post saved successfully");
+                        break;
+                    case 2:
+                        Console.WriteLine("You choosed Delete post");
+                        Console.WriteLine(" Last post deleted successfully");
+                        break;
+                    case 3:
+                        Console.WriteLine("You choosed Show all posts. Listed below already existing records");
+                        string a = "1998";
+                        string c = "1876";
+                        Console.WriteLine(a);
+                        Console.WriteLine(c);
+                        break;
+                }
+            } while (userReaction != 4);
         }
     }
 }
