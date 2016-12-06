f1282eb08b4407c7e60f59af54f60704356524fe
Serhiy Buta <otaman963@gmail.com>
2016-12-06 15:31:27 +0200

    Added check for not vailed number

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -9,44 +9,60 @@ namespace TodoList
 {
     public class Program
     {
+        static void DoAction(NoteBoard board , int userReaction)
+        {
+            switch (userReaction)
+            {
+                case 1:
+                    board.WriteNote();
+                    break;
+                case 2:
+                    board.DeleteNote();
+                    break;
+                case 3:
+                    board.ShowNote();
+                    break;
+                case 4:
+                    board.CheckNote();
+                    break;
+            }
+        }
+        static void ShowFunction()
+        {
+            Console.WriteLine(
+                "------------------------------- \n" +
+                " The program has a functional : \n\n" +
+                " 1. Add post ; \n" +
+                " 2. Delete post ; \n" +
+                " 3. Show all posts ; \n" +
+                " 4. Check note ; \n" +
+                " 5. Exit ; \n\n" +
+                " Please enter the number :");
+        }
+
         static void Main(string[] args)
         {
             int userReaction = 0;
-
             var board = new NoteBoard();
 
             do
             {
-                Console.WriteLine(
-                    "------------------------------- \n" +
-                    " The program has a functional : \n\n" +
-                    " 1. Add post ; \n" +
-                    " 2. Delete post ; \n" +
-                    " 3. Show all posts ; \n" +
-                    " 4. Check note ; \n" +
-                    " 5. Exit ; \n\n" +
-                    " Please enter the number :");
-
-                userReaction = int.Parse(Console.ReadLine());
-                Console.WriteLine();
-
-                switch (userReaction)
+                try
                 {
-                    case 1:
-                        board.WriteNote();
-                        break;
-                    case 2:
-                        board.DeleteNote();
-                        break;
-                    case 3:
-                        board.ShowNote();
-                        break;
-                    case 4:
-                        board.CheckNote();
-                        break;
+                    ShowFunction();
+
+                    userReaction = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    DoAction(board, userReaction);
                 }
-            }
-            while (userReaction != 5);
+                catch (Exception)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(" You entered not correct number !!! ");
+                    Console.WriteLine();
+                }
+            } while (userReaction != 5);
         }
     }
 }
