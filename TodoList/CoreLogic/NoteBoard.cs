5fc85f90adc4bc44a3280d2edc54d99c4d0ca170
Serhiy Buta <otaman963@gmail.com>
2016-12-06 14:50:58 +0200

    Code refactoring

--- TodoList/CoreLogic/NoteBoard.cs
+++ TodoList/CoreLogic/NoteBoard.cs
@@ -9,5 +9,51 @@ namespace CoreLogic
     public class NoteBoard
     {
         public List<Note> Notes = new List<Note>();
+
+        public void CheckNote()
+        {
+            Console.WriteLine("Check note");
+            var choseNote = Console.ReadLine();
+            var number = Int32.Parse(choseNote);
+            Notes[number - 1].Checkbox = CheckboxState.Checked;
+        }
+
+        public void ShowNote()
+        {
+            Console.WriteLine("You choosed Show all posts. Listed below already existing records");
+            foreach (var n in Notes)
+            {
+                if (n.Checkbox == CheckboxState.Checked)
+                {
+                    Console.WriteLine(" [x] " + n.Record);
+                }
+                else
+                {
+                    Console.WriteLine(" [ ] " + n.Record);
+                }
+            }
+        }
+
+        public void DeleteNote()
+        {
+            Console.WriteLine("You choosed Delete post");
+
+            Notes.Remove(Notes.Last());
+            Console.WriteLine(" Last post deleted successfully");
+        }
+
+        public void WriteNote()
+        {
+            Console.WriteLine("You choosed Add post. Enter your line below");
+            var enteredLine = Console.ReadLine();
+
+            var note = new Note();
+            note.Checkbox = CheckboxState.Unchecked;
+            note.Record = enteredLine;
+            Notes.Add(note);
+
+            Console.WriteLine("You wrote: {0}", enteredLine);
+            Console.WriteLine("post saved successfully");
+        }
     }
 }
--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -11,12 +11,10 @@ namespace TodoList
     {
         static void Main(string[] args)
         {
-//            var note = new Note();
-//            note.Checkbox = CheckboxState.Unchecked;
-            var board = new NoteBoard();
-//            board.Notes.Add(note);
-
             int userReaction = 0;
+
+            var board = new NoteBoard();
+
             do
             {
                 Console.WriteLine(
@@ -35,43 +33,17 @@ namespace TodoList
                 switch (userReaction)
                 {
                     case 1:
-                        Console.WriteLine("You choosed Add post. Enter your line below");
-                        var enteredLine = Console.ReadLine();
-
-                        var note = new Note();
-                        note.Checkbox = CheckboxState.Unchecked;
-                        note.Record = enteredLine;
-                        board.Notes.Add(note);
-
-                        Console.WriteLine("You wrote: {0}", enteredLine);
-                        Console.WriteLine("post saved successfully");
+                        board.WriteNote();
                         break;
                     case 2:
-                        Console.WriteLine("You choosed Delete post");
-                        
-                        board.Notes.Remove(board.Notes.Last());
-                        Console.WriteLine(" Last post deleted successfully");
+                        board.DeleteNote();
                         break;
                     case 3:
-                        Console.WriteLine("You choosed Show all posts. Listed below already existing records");
-                        foreach (var n in board.Notes)
-                        {
-                            if ( n.Checkbox == CheckboxState.Checked)
-                            {
-                                Console.WriteLine(" [x] " + n.Record);
-                            }
-                            else
-                            {
-                                Console.WriteLine(" [ ] " + n.Record);
-                            }
-                        }
+                        board.ShowNote();
                         break;
                     case 4:
-                        Console.WriteLine("Check note");
-                        var choseNote = Console.ReadLine();
-                        var number = int.Parse(choseNote);
-                        board.Notes[number - 1].Checkbox = CheckboxState.Checked;
-                        break;;
+                        board.CheckNote();
+                        break;
                 }
             }
             while (userReaction != 5);
